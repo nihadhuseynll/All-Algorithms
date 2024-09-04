4d1dc9fb566982d8e01ad684a32dc3c226482d00
Nikoli <huseynlinihad4400@gmail.com>
2024-09-04 16:56:23 +0400

    Breadth First Search Algorithm

--- All Algorithms/Program.cs
+++ All Algorithms/Program.cs
@@ -80,67 +80,136 @@ namespace All_Algorithms
 
 		// 3.Depth First Search Algorithm
 
-		static void AddEdge(List<List<int>> adj,int s,int t)
-		{
-			adj[s].Add(t);
-			adj[t].Add(s);
-		}
+		//static void AddEdge(List<List<int>> adj, int s, int t)
+		//{
+		//	adj[s].Add(t);
+		//	adj[t].Add(s);
+		//}
 
-		// Recursive function for DFS traversal
-		static void DfsRec(List<List<int>> adj, bool[] visited,int s)
+		//// Recursive function for DFS traversal
+		//static void DfsRec(List<List<int>> adj, bool[] visited, int s)
+		//{
+		//	// Mark the current vertex as visited
+		//	visited[s] = true;
+
+		//	// Print the current vertex
+		//	Console.Write(s + " ");
+
+		//	// Recursively visit all adjacent vertices
+		//	// that are not visited yet
+		//	foreach (int i in adj[s])
+		//	{
+		//		if (!visited[i])
+		//		{
+		//			DfsRec(adj, visited, i);
+		//		}
+		//	}
+		//}
+		//// Main DFS function that initializes the visited array
+		//static void PerformDfs(List<List<int>> adj, int s)
+		//{
+		//	bool[] visited = new bool[adj.Count];
+		//	// Call the recursive DFS function
+		//	DfsRec(adj, visited, s);
+		//}
+
+		//static void Main(string[] args)
+		//{
+		//	int v = 5;
+
+		//	// Create an adjancency list for the graph
+		//	List<List<int>> adj = new List<List<int>>(v);
+
+		//	for (int i = 0; i < v; i++)
+		//	{
+		//		adj.Add(new List<int>());
+		//	}
+		//	// Define the edges of the graph
+		//	int[,] edges =
+		//	{
+		//		{1,2 },
+		//		{1,0 },
+		//		{2,0 },
+		//		{2,3 },
+		//		{2,4 }
+		//	};
+		//	// Populate the adjacency list with edges
+		//	for (int i = 0; i < edges.GetLength(0); i++)
+		//	{
+		//		AddEdge(adj, edges[i, 0], edges[i, 1]);
+		//	}
+		//	int source = 2; // Starting vertex for DFS
+		//	Console.WriteLine("DFS from source - " + source);
+		//	PerformDfs(adj, source);
+
+		//	Console.ReadLine();
+		//}
+
+		//4.Breadth First Search Algorithm
+
+		static void BFS(List<List<int>> adj, int s, bool[] visited)
 		{
-			// Mark the current vertex as visited
+			// Create a queue for BFS
+			Queue<int> q = new Queue<int>();
+
+			// Mark the source node as visited and enqueue it
 			visited[s] = true;
+			q.Enqueue(s);
 
-			// Print the current vertex
-			Console.Write(s + " ");
-
-			// Recursively visit all adjacent vertices
-			// that are not visited yet
-			foreach(int i in adj[s])
+			// Iterate over the queue
+			while (q.Count > 0)
 			{
-				if (!visited[i])
+				// Dequeue a vertex from queue and print it
+				int curr = q.Dequeue();
+				Console.Write(curr + " ");
+
+				// Get all adjacent vertices of the dequeued vertex
+				// If an adjacent has not been visited, 
+				// mark it visited and enqueue it
+				foreach (int i in adj[curr])
 				{
-					DfsRec(adj, visited, i);
+					if (!visited[i])
+					{
+						visited[i] = true;
+						q.Enqueue(i);
+					}
 				}
 			}
 		}
-		// Main DFS function that initializes the visited array
-		static void PerformDfs(List<List<int>> adj, int s)
+
+		// Function to add an edge to the graph
+		static void AddEdge(List<List<int>> adj, int u, int v)
 		{
-			bool[] visited = new bool[adj.Count];
-			// Call the recursive DFS function
-			DfsRec(adj,visited,s);
+			adj[u].Add(v);
+			adj[v].Add(u);
 		}
 
 		static void Main(string[] args)
 		{
-			int v = 5;
+			// Number of vertices in the graph
+			int V = 5;
 
-			// Create an adjancency list for the graph
-			List<List<int>> adj = new List<List<int>>(v);
+			// Adjacency list representation of the graph
+			List<List<int>> adj = new List<List<int>>(V);
+			for (int i = 0; i < V; i++)
+			{
+				adj.Add(new List<int>());
+			}
 
-			for(int i=0; i < v; i++)
+			// Add edges to the graph
+			int[,] edges = { { 0, 1 }, { 0, 2 }, { 1, 3 }, { 1, 4 }, { 2, 4 } };
+
+			for (int i = 0; i < edges.GetLength(0); i++)
 			{
-				adj.Add(new List<int>());	
+				AddEdge(adj, edges[i, 0], edges[i, 1]);
 			}
-			// Define the edges of the graph
-			int[,] edges =
-			{
-				{1,2 },
-				{1,0 },
-				{2,0 },
-				{2,3 },
-				{2,4 }
-			};
-			// Populate the adjacency list with edges
-			for(int i = 0; i < edges.GetLength(0); i++)
-			{
-				AddEdge(adj, edges[i, 0], edges[i,1]);
-			}
-			int source = 2; // Starting vertex for DFS
-			Console.WriteLine("DFS from source - " + source);
-			PerformDfs(adj, source);
+
+			// Mark all the vertices as not visited
+			bool[] visited = new bool[V];
+
+			// Perform BFS traversal starting from vertex 0
+			Console.WriteLine("BFS starting from 0 : ");
+			BFS(adj, 0, visited);
 
 			Console.ReadLine();
 		}
